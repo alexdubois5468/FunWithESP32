07317151b05791a8a676ceb3ae68d43760bbbe6d
alexdubois5468 <76630359+alexdubois5468@users.noreply.github.com>
2022-11-30 16:00:43 -0500

    FirstCommit

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,19 @@
 #include <Arduino.h>
 
+#define RLED 16
+
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(9600);//Set the baudrate to 115200,same as the software settings
+  Serial.begin(115200);//Set the baudrate to 115200,same as the software settings
+  pinMode(RLED, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  Serial.println("Hello World!");//Print character string“Hello World！”on the serial
-  delay(5000);// Delay 5 second
+  uint32_t f = esp_get_cpu_freq_mhz();
+  Serial.println(f); // Print CPU Freq on the serial
+  digitalWrite(RLED, HIGH);
+  delay(2500);// Delay 2.5 second
+  digitalWrite(RLED, LOW);
+  delay(2500);// Delay 2.5 second
 }
